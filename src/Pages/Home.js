dc7450a66ab7a3db1d824cb8f6e40b20f28a936b
Mukund Gohil <gohil.mukund@gmail.com>
2020-08-31 12:17:00 +0530

    Modified home page

---
+++ src/Pages/Home.js
@@ -0,0 +1,39 @@
+import React from 'react';
+// import logo from '../assets/Images/logo.svg';
+import '../Styles/App.css';
+import '../Styles/star.css';
+
+const App = () => (
+  <div>
+    
+      <div id='container' style={{height:"100vh", width:"100vh", overflow:"hidden"}} >
+          <link href='https://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css' />
+          <div id='stars'></div>
+          <div id='stars2'></div>
+          <div id='stars3'></div>
+          <div id='title'>
+            <span>
+              MUKUND GOHIL
+            </span>
+            <br />
+            <span>
+              &
+            </span>
+            <br />
+            <span>
+              KHUSHBU SOLANKI
+            </span>
+            <br />
+            <a type="button" href="#text" > Hello </a>
+          </div>
+      
+    </div>
+
+    <div id="text" height='100vh'>
+        <h1> Hello World</h1>
+    </div>
+
+  </div>
+);
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './Pages/Home';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDat7NFSCyGQs801U5rIVm66euKgr12AT4",
+  authDomain: "mukund-fun.firebaseapp.com",
+  databaseURL: "https://mukund-fun.firebaseio.com",
+  projectId: "mukund-fun",
+  storageBucket: "mukund-fun.appspot.com",
+  messagingSenderId: "575650076396",
+  appId: "1:575650076396:web:623b6b2bb7ee5a39b2b7c9",
+  measurementId: "G-RJ6MKGVXX4"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
