9f22bdf887e138f9ca34d74dbc567f2e5faf412a
Mukund Gohil <gohil.mukund@gmail.com>
2020-12-10 03:13:01 +0530

    minor Fix

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,7 +6,7 @@ import '../Styles/star.css';
 const App = () => (
   <div>
     
-      <div id='container' style={{height:"100vh", width:"100vh", overflow:"hidden"}} >
+      <div id='container' style={{height:"100vh", overflow:"hidden"}} >
           <link href='https://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css' />
           <div id='stars'></div>
           <div id='stars2'></div>
